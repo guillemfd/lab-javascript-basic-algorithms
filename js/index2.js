3ce542902265a12f63bcfd40209f1cbbb19b0429
Guillem Ferrer <guillemfd@gmail.com>
2022-05-31 13:10:47 +0200

    JUST DO IT ALL AGAIN

--- js/index2.js
+++ js/index2.js
@@ -1,5 +1,5 @@
 // Iteration 1: Names and Input
-
+//v1
 const hacker1 = 'Guillem'
     console.log(`The driver's name is ${hacker1}`)
 
@@ -7,7 +7,18 @@ const hacker2 = 'Bernat'
     console.log(`The navigator's name is ${hacker2}`)
 
 
+//v2
+const hacker10 = 'Guillem'
+    console.log(`The driver's name is ${hacker10}`)
+
+const hacker20 = 'Bernat'
+console.log(`The navigator's 's name is ${hacker20}`)
+//--------------------------------------------------------------------------------------------------------------------------------------
+
+
+
 // Iteration 2: Conditionals
+console.log('Ex2    <----------------------------------------')
 //v1
 if (hacker1.length > hacker2.length) {
     console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
@@ -30,10 +41,24 @@ if (hacker1.length > hacker2.length) {
 console.log('Ex. 2 V2 END')
 
 
+//v3
+if (hacker10.length > hacker20.length) {
+    console.log(`The driver has the longest name, it has ${hacker10.length} characters.`)
+} else if  (hacker10.length > hacker20.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker20.length} characters.`)
+} else {
+    console.log(`Wow, you both have equally long names, ${hacker20.length} characters!`)
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------------
+
 
 
 
 //Iteration 3: Loops
+
+//3.1
+console.log('3.1  <----------------------------------------')
 //v1
         console.log('Ex. 3 V1 START')
         let newDriver = ''
@@ -45,49 +70,15 @@ console.log('Ex. 2 V2 END')
         console.log('Ex. 3 V1 END')
 
 //v2
-        console.log('Ex. 3 V2 START')
+        console.log('Ex. 3.1 V2 START')
                 let longName = ''
                 for (i=0; i<hacker1.length; i++) {
                     longName += hacker1[i] + ' '
                 }
                 console.log(longName)
-        console.log('Ex. 3 V2 END')
+        console.log('Ex. 3.1 V2 END')
 
-
-// for (let i = 0; i < hacker1.length; i++) {
-//     console.log(hacker1.charAt[i].toUpperCase)
-// }
-
-// console.log('Y AHORA AL REVÉS')
-
-// for (let i = sentence.length - 1; i >= 0; i--) {
-//     console.log(`El caracter es ${sentence.charAt(i)}`)
-// }
-
-
-
-
-
-
-//1
-const hacker10 = 'Guillem'
-    console.log(`The driver's name is ${hacker10}`)
-
-const hacker20 = 'Bernat'
-console.log(`The navigator's 's name is ${hacker20}`)
-
-
-//2
-if (hacker10.length > hacker20.length) {
-    console.log(`The driver has the longest name, it has ${hacker10.length} characters.`)
-} else if  (hacker10.length > hacker20.length) {
-    console.log(`It seems that the navigator has the longest name, it has ${hacker20.length} characters.`)
-} else {
-    console.log(`Wow, you both have equally long names, ${hacker20.length} characters!`)
-}
-
-
-//3.1
+//v3
 let capsName = ''
 
 for (let i = 0; i < hacker10.length ; i++) {
@@ -97,23 +88,86 @@ for (let i = 0; i < hacker10.length ; i++) {
 console.log(capsName)
 
 
-
 //3.2
+console.log('3.2 <----------------------------------------')
+
+//v1
+console.log('Ex. 3.2 V1 START')
+    let reverseNav = ''
+    for (let i=hacker2.length -1; i>=0; i--) {
+            reverseNav += hacker2[i]
+    }
+    console.log(reverseNav)
+console.log('Ex. 3.2 V1 END')
+
+
+
+// console.log('Y AHORA AL REVÉS')
+
+// for (let i = sentence.length - 1; i >= 0; i--) {
+//     console.log(`El caracter es ${sentence.charAt(i)}`)
+// }
+
+
+//v2
+console.log('Ex. 3.2 V2 START')
 let reverse = ''
 for (let i = hacker20.length -1; i >= 0; i--) {
     reverse += hacker20[i] 
 }
 
 console.log(reverse)
+console.log('Ex. 3.2 V2 END')
 
 
 
+//3.3
+console.log('3.3 <---------------------------------------- 3.3')
+//v1
+console.log('Ex. 3.3 V1 START')
+if (hacker1 < hacker2) {
+    console.log(hacker1 + ', ' + hacker2)
+} else if (hacker1 > hacker2) {
+    console.log(hacker2 + ', ' + hacker1)
+} else {
+    console.log('What?! You both have the same name?')
+}
+console.log('Ex. 3.3 V1 END')
+
+
+// //v2
+// console.log('Ex. 3.3 V2 START')
+// let lexicOrder = 'abcdefghijklmnopqrstuvwxyz'
+// if (hacker1 === hacker2) {
+//     console.log('What?! You both have the same name?')
+// } else {
+//     for (let index = 0; index < lexicOrder.length; index++) {
+//         if (hacker1[0] === lexicOrder[index]) {
+//           console.log(hacker1 + ", " + hacker2);
+//           index = lexicOrder.length;
+//         } else if (hacker2[0] === lexicOrder[index]) {
+//           console.log(hacker2 + ", " + hacker1);
+//           index = lexicOrder.length;
+//         }
+//       }
+//     }
+// console.log('Ex. 3.3 V2 END')
+
+console.log('Ex. 3.3 V2 START')
+//The localeCompare() method returns sort order -1, 1, or 0 (for before, after, or equal)
+if (coder1.localeCompare(coder2) === -1) {  //és a dir, si la paraula del 'coder1' va ABANS que el 'coder2' 
+    console.log(`The driver's name goes first.`)
+} else if (coder1.localeCompare(coder2) === 1) {  //és a dir, si la paraula del 'coder1' va DESPRÉS que el 'coder2'
+    console.log(`Yo, the navigator's name goes first definitely.`)
+} else {
+    console.log(`What?! You both have the same name?`)}
+console.log('Ex. 3.3 V2 END')
 
 
 //----------------------------------------------------------------------------------------
 
 
-console.log('----------------------------->   JUST DO IT AGAIN   <--------------------------------')
+console.log('----------------------------->   JUST DO IT ALL AGAIN   <--------------------------------')
 //1
 const coder1 = 'Guillem'
 const coder2 = 'Bernat'
